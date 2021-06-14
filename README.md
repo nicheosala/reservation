cfc2d4804e3b156ef4f88934672f4bc3e4e2a753
nicheosala <48255219+nicheosala@users.noreply.github.com>
2021-06-14 12:24:43 +0200

    First version of Reservation contract.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reservation
+
+This is a smart contract for managing the booking of an apartment.
+In short, by implementing a typical functionality on the blockchain such as free cancellation of a reservation, the need to place one's trust in services such as Booking or AirBnb would be eliminated, favoring a direct financial relationship between the buyer and the owner of the apartment.
+For the owner of an apartment it would mean eliminating or at least reducing the fees to be paid for the aforementioned services, with a clear economic advantage, while the buyer could make reservations with greater carefree.
---
+++ tests/Reservation_test.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.4;
+
+import "remix_tests.sol"; 
+import "remix_accounts.sol";
+import "../contracts/Reservation.sol";
+
+contract tests {
+
+    Reservation reservation;
+    uint constant testCost = 100;
+    uint constant testStartDate = 1627776000;
+    address testOwner;
+    
+    // Accept any incoming amount.
+    receive () external payable {}
+    
+    function beforeAll() public {
+        reservation = new Reservation(testStartDate, testCost);
+    }
+    
+    function testConstructor() public {
+        Assert.equal(reservation.reserver(), payable(address(0)),
+        'The reserver should be empty.');
+    }
+
+    function testIsBooked() public {
+        Assert.equal(reservation.isBooked(), false,
+        'The reservation should not be booked by default.');
+    }
+
+    /// #value: 100
+    function testBook() public payable {
+        reservation.book{value: 100}();
+        Assert.equal(reservation.isBooked(), true,
+        'The reservation should be booked after a valid call to book().');
+    }
+    
+    /// #value: 100
+    function testAlreadyBooked() public payable {
+        try reservation.book{value: 100}() {
+            /* Nothing to do here: we expect an error. */
+        } catch Error(string memory reason) {
+            Assert.equal(reason, 'The apartment is already booked.',
+            'Booking a reservation that is already booked should not be allowed.');
+        }
+    }
+    
+    /// #value: 200
+    function testInvalidBookValue() public payable {
+        try reservation.book{value: 200}() {
+            /* Nothing to do here: we expect an error. */
+        } catch Error(string memory reason) {
+            Assert.equal(reason, 'The reserver should pay exactly the cost of the apartment.',
+            'Booking an apartment not paying the exact cost should not be allowed.');
+        }
+    }
+    
+    function testCancel() public {
+        try reservation.cancel() {
+            Assert.equal(reservation.isBooked(), false,
+            'The reservation should not be booked after a valid call to cancel().');
+        } catch Error(string memory reason) {
+            Assert.equal(reason, 'The reserver should pay exactly the cost of the apartment.',
+            'Booking an apartment not paying the exact cost should not be allowed.');
+        } catch (bytes memory /*lowLevelData*/) {
+            Assert.ok(false, 'failed unexpected');
+        }
+    }
+    
+    function testInvalidCancel() public {
+        try reservation.cancel() {
+            /* Nothing to do here: we expect an error. */
+        } catch Error(string memory reason) {
+            Assert.equal(reason, 'The apartment is not booked by you.',
+            'Cancelling a reservation that is not booked should not be allowed.');
+        }
+    }
+}
